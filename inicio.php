2d3f2e68f89ad203122af0b2919d3c50c296ec1a
Mario Rivera <maro.arivera@gmail.com>
2016-06-26 00:13:12 -0500

    Update

--- inicio.php
+++ inicio.php
@@ -4,5 +4,6 @@ echo "Hola desde php!";
 $edicion = "editado";
 echo "este es un archivo ".$edicion;
 
-
+//Se pretende obtener la informacion conseguida de la conexion a BD
+$sql = "SELECT * FROM tabla1 WHERE id = ".$id;
 ?>
